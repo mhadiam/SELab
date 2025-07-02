23f4e7b2730351e901721ae56fc4e452d0a64312
mahdi hadi <mahdihadiam@gmail.com>
2025-07-02 20:04:56 +0330

    adapter implementation

--- GraphTraverser/01 - Adapter Implementation/src/main/java/org/example/adapters/GraphAdapter.java
+++ GraphTraverser/01 - Adapter Implementation/src/main/java/org/example/adapters/GraphAdapter.java
@@ -1,4 +1,9 @@
 package org.example.adapters;
 
-public class GraphAdapter {
+import java.util.Set;
+
+public interface GraphAdapter<V> {
+    void addVertex(V vertex);
+    void addEdge(V source, V target);
+    Set<V> getNeighbors(V vertex);
 }
--- GraphTraverser/01 - Adapter Implementation/src/main/java/org/example/adapters/JGraphTGraphAdapter.java
+++ GraphTraverser/01 - Adapter Implementation/src/main/java/org/example/adapters/JGraphTGraphAdapter.java
@@ -1,4 +1,40 @@
 package org.example.adapters;
 
-public class JGraphTGraph {
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class JGraphTGraphAdapter<V> implements GraphAdapter<V> {
+    private final Graph<V, DefaultEdge> graph;
+
+    public JGraphTGraphAdapter(Graph<V, DefaultEdge> graph) {
+        this.graph = graph;
+    }
+
+    @Override
+    public void addVertex(V vertex) {
+        graph.addVertex(vertex);
+    }
+
+    @Override
+    public void addEdge(V source, V target) {
+        graph.addEdge(source, target);
+    }
+
+    @Override
+    public Set<V> getNeighbors(V vertex) {
+        Set<V> neighbors = new HashSet<>();
+        for (DefaultEdge edge : graph.edgesOf(vertex)) {
+            V source = graph.getEdgeSource(edge);
+            V target = graph.getEdgeTarget(edge);
+            if (!vertex.equals(source)) {
+                neighbors.add(source);
+            } else {
+                neighbors.add(target);
+            }
+        }
+        return neighbors;
+    }
 }
